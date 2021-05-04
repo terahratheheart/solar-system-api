64661b34a2ec94b726db728b369ef0648d885649
brew doctor <brew upgrade python@3.9>
2021-05-04 10:43:24 -0700

    added tests routes

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from app import create_app
+from app import db
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
